f0b1c902527ba33c7db00cfc8204a38f3a707f94
Hizbul Bahar <hizbul@ergo-ventures.com>
2018-05-15 12:40:06 +0600

    pos_multi_session_restaurant/

---
+++ pos_multi_session_restaurant/__manifest__.py
@@ -0,0 +1,37 @@
+{
+    "name": """Sync restaurant orders""",
+    "summary": """Staff get order details immediately after waiter taps on tablet""",
+    "category": "Point of Sale",
+    "live_test_url": 'http://apps.it-projects.info/shop/product/pos-multi-session?version=11.0',
+    "images": ['images/s2.png'],
+    "version": "11.0.2.2.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Ivan Yelizariev",
+    "support": "apps@it-projects.info",
+    "website": "https://yelizariev.github.io",
+    "license": "LGPL-3",
+    "price": 30.00,
+    "currency": "EUR",
+
+    "depends": [
+        "pos_restaurant",
+        "pos_multi_session",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "views/views.xml",
+    ],
+    "qweb": [
+    ],
+    "demo": [
+        "demo/demo.xml",
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": True,
+    "installable": True,
+}
